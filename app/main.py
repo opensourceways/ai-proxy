b4491e272509b4f6150f04a0a435fdd6a483bf71
mccpr <cuiming1990@163.com>
2024-11-12 17:23:17 +0800

    update test

--- app/main.py
+++ app/main.py
@@ -1,17 +1,27 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
 
 from api.v1 import chat,embeddings
 from config import settings
 
 app = FastAPI()
 
-app.include_router(chat.router, prefix="/chat", tags=["chat"])
-app.include_router(embeddings.router, prefix="/embeddings", tags=["embeddings"])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def main():
-    uvicorn.run(app, host=settings.HOST, port=settings.PORT)
+def verify_token(token: str = Depends(oauth2_scheme)):
+    if token != settings.VALID_TOKEN:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or missing token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return token
+
+app.include_router(chat.router, prefix="/chat", tags=["chat"], dependencies=[Depends(verify_token)])
+app.include_router(embeddings.router, prefix="/embeddings", tags=["embeddings"], dependencies=[Depends(verify_token)])
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host=settings.HOST, port=settings.PORT)
--- app/scripts/test.sh
+++ app/scripts/test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+env_path="$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")"
+env_file=${env_path}/.env
+source ${env_file}
 echo "model 1 start "
-curl http://127.0.0.1:8000/chat/completions -H "Content-Type: application/json" -d '{"model": "Qwen2.5-32B-Instruct","prompt": "San Francisco is a","max_tokens": 512,"temperature": 0}'
+curl http://127.0.0.1:8000/chat/completions -H "Content-Type: application/json" -H "Authorization: Bearer ${VALID_TOKEN}" -d '{"model": "Qwen2.5-32B-Instruct","prompt": "San Francisco is a","max_tokens": 512,"temperature": 0}'
+echo ""
 echo "model 2 start"
-curl http://127.0.0.1:8000/chat/completions -H "Content-Type: application/json" -d '{"model": "Qwen2.5-14B-Instruct","prompt": "San Francisco is a","max_tokens": 512,"temperature": 0}'
+curl http://127.0.0.1:8000/chat/completions -H "Content-Type: application/json" -H "Authorization: Bearer ${VALID_TOKEN}" -d '{"model": "Qwen2.5-14B-Instruct","prompt": "San Francisco is a","max_tokens": 512,"temperature": 0}'
+echo ""
 echo "model 3 start"
-curl http://127.0.0.1:8000/embeddings -H "Content-Type: application/json" -d '{"prompt": "The food was delicious and the waiter...","model": "bge-large-en-v1.5","encoding_format": "float"}'
+curl http://127.0.0.1:8000/embeddings -H "Content-Type: application/json" -H "Authorization: Bearer ${VALID_TOKEN}" -d '{"prompt": "The food was delicious and the waiter...","model": "bge-large-en-v1.5","encoding_format": "float"}'
+echo ""
 echo "model test end"
